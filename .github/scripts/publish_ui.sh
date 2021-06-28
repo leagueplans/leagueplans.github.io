63421eee7761ac5d235d8b179692c65b34e3f0a3
Daniel Moss <daniel.david.moss+git@gmail.com>
2021-06-28 01:51:43 +0100

    #1: Patch actions

---
+++ .github/scripts/publish_ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# Pushes updated versions of main.js and index.html to
+# the docs folder, for use as github-pages.
+
+sbt ui/fullLinkJS
+cp ui/index-dev.html docs/index.html
+mv ui/target/scala-3.0.0/ui-opt/main.js docs/main.js
+sed -i 's/(?<=src=").*(?=")/main\.js/g' docs/index.html
+
+git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
+git config --local user.name 'github-actions[bot]'
+git commit -am 'Publish github-pages'
+git push
