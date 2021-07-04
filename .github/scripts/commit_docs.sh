67d4a294a0d7aebc368b6518917bcc388e7dc13b
Daniel Moss <daniel.david.moss+git@gmail.com>
2021-07-04 21:29:32 +0100

    #5: Only attempt to push if something has changed

--- .github/scripts/commit_docs.sh
+++ .github/scripts/commit_docs.sh
@@ -3,8 +3,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
-git config --local user.name 'github-actions[bot]'
 git add 'docs'
-git commit --message='Publish github-pages'
-git push
+
+if [[ `git status --porcelain --untracked-files=no` ]]; then
+  git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
+  git config --local user.name 'github-actions[bot]'
+  git commit --message='Publish github-pages'
+  git push
+fi
