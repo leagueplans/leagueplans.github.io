2cfd54c0cb96e2367f910929c90ac31ef3a557aa
Daniel Moss <daniel.david.moss+git@gmail.com>
2021-07-04 22:29:38 +0100

    #5: Test raising a PR after scraping

--- scraper/src/main/scala/ddm/scraper/skillicons/Main.scala
+++ scraper/src/main/scala/ddm/scraper/skillicons/Main.scala
@@ -3,9 +3,15 @@ package ddm.scraper.skillicons
 import ddm.scraper.core.{PageFetcher, Scraper}
 import net.ruippeixotog.scalascraper.browser.HtmlUnitBrowser
 
-import java.nio.file.Path
+import java.nio.file.{Files, Path}
+import scala.jdk.CollectionConverters.IterableHasAsJava
 
 object Main extends Scraper {
-  def run(pageFetcher: PageFetcher[HtmlUnitBrowser], targetDirectory: Path): Unit =
+  def run(pageFetcher: PageFetcher[HtmlUnitBrowser], targetDirectory: Path): Unit = {
     pageFetcher.fetch("File:Attack_icon.png#file")
+    Files.write(
+      targetDirectory.resolve("test.txt"),
+      List("Some data to store in the file").asJava
+    )
+  }
 }
