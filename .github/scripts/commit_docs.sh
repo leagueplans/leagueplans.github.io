7ce8dff651bd890c3ffb82cc6ba20834fb51af73
Daniel Moss <daniel.david.moss+git@gmail.com>
2021-07-04 21:47:48 +0100

    #5: Only check  the docs folder for changes

--- .github/scripts/commit_docs.sh
+++ .github/scripts/commit_docs.sh
@@ -3,9 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-git add 'docs'
+readonly DOCS='docs'
+git add "${DOCS}"
 
-if [[ `git status --porcelain --untracked-files=no` ]]; then
+if [[ `git status "${DOCS}" --porcelain --untracked-files=no` ]]; then
   git config --local user.email '41898282+github-actions[bot]@users.noreply.github.com'
   git config --local user.name 'github-actions[bot]'
   git commit --message='Publish github-pages'
