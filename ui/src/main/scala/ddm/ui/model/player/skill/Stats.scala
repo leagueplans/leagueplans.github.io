de35d6f34531d048d38c4a63c647724d04f91d53
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-01-11 19:06:59 +0000

    #28: Add support for combat levels

--- ui/src/main/scala/ddm/ui/model/player/skill/Stats.scala
+++ ui/src/main/scala/ddm/ui/model/player/skill/Stats.scala
@@ -23,4 +23,21 @@ final case class Stats(raw: Map[Skill, Exp]) {
       .all
       .map(apply)
       .reduce(_ + _)
+
+  lazy val combatLevel: Double = {
+    val attack = Level.of(this(Attack)).raw
+    val strength = Level.of(this(Strength)).raw
+    val defence = Level.of(this(Defence)).raw
+    val hitpoints = Level.of(this(Hitpoints)).raw
+    val prayer = Level.of(this(Prayer)).raw
+    val ranged = Level.of(this(Ranged)).raw
+    val magic = Level.of(this(Magic)).raw
+
+    val base = 0.25 * (defence + hitpoints + Math.floor(prayer / 2.0))
+    val melee = attack + strength
+    val range = ranged * 3.0 / 2.0
+    val mage = magic * 3.0 / 2.0
+
+    base + (13.0 / 40.0 * Math.max(Math.max(melee, range), mage))
+  }
 }
