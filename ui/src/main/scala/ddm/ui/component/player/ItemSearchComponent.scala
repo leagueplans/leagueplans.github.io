763cda7110d7140f6d8166a67d43baecc592d92a
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-01-14 00:28:10 +0000

    #20: Throttle fuse queries

--- ui/src/main/scala/ddm/ui/component/player/ItemSearchComponent.scala
+++ ui/src/main/scala/ddm/ui/component/player/ItemSearchComponent.scala
@@ -7,24 +7,37 @@ import japgolly.scalajs.react.component.Scala.Component
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{BackendScope, Callback, CtorType, ScalaComponent}
 
+import scala.concurrent.duration.DurationInt
+import scala.scalajs.js.timers
+import scala.scalajs.js.timers.SetTimeoutHandle
+
 object ItemSearchComponent {
   val build: Component[Props, State, Backend, CtorType.Props] =
     ScalaComponent
       .builder[Props]
-      .initialState[State](State(query = ""))
+      .initialState[State](State(
+        searchInput = "",
+        fuseQuery = "",
+        inputUpdateTimer = None
+      ))
       .renderBackend[Backend]
       .build
 
   type Props = Fuse[Item]
-  final case class State(query: String)
+
+  final case class State(
+    searchInput: String,
+    fuseQuery: String,
+    inputUpdateTimer: Option[SetTimeoutHandle]
+  )
 
   final class Backend(scope: BackendScope[Props, State]) {
     def render(itemFuse: Fuse[Item], state: State): VdomElement =
       <.div(
-        SearchBoxComponent.build((state.query, updateQuery)),
+        SearchBoxComponent.build((state.searchInput, updateInput)),
         <.ul(
           itemFuse
-            .search(state.query, limit = 10)
+            .search(state.fuseQuery, limit = 10)
             .toTagMod(item =>
               <.li(
                 ^.key := item.id.raw,
@@ -34,8 +47,23 @@ object ItemSearchComponent {
         )
       )
 
-    private def updateQuery(query: String): Callback =
-      scope.modState(_.copy(query = query))
+    private def updateInput(input: String): Callback =
+      scope.modState { current =>
+        current.inputUpdateTimer.foreach(timers.clearTimeout)
+
+        current.copy(
+          searchInput = input,
+          inputUpdateTimer = Some(timers.setTimeout(250.milliseconds)(updateQuery.runNow()))
+        )
+      }
+
+    private val updateQuery: Callback =
+      scope.modState { current =>
+        current.copy(
+          fuseQuery = current.searchInput,
+          inputUpdateTimer = None
+        )
+      }
 
     private def renderItem(item: Item): VdomElement =
       ElementWithTooltipComponent.build(
