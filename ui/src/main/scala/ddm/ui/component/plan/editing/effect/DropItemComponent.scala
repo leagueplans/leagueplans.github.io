25a0865eb963866d536bf156e748501cf20cb3e5
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-01-17 22:22:00 +0000

    #73: Move logic for selecting an item from a depository to its own component

--- ui/src/main/scala/ddm/ui/component/plan/editing/effect/DropItemComponent.scala
+++ ui/src/main/scala/ddm/ui/component/plan/editing/effect/DropItemComponent.scala
@@ -1,10 +1,10 @@
 package ddm.ui.component.plan.editing.effect
 
-import ddm.ui.component.With
-import ddm.ui.component.common.form.{FormComponent, NumberInputComponent, SelectComponent}
+import ddm.ui.component.common.form.FormComponent
+import ddm.ui.component.plan.editing.effect.SelectItemFromDepositoryComponent.SelectedItem
 import ddm.ui.model.plan.Effect.DropItem
 import ddm.ui.model.player.Player
-import ddm.ui.model.player.item.{Depository, Item, ItemCache}
+import ddm.ui.model.player.item.ItemCache
 import japgolly.scalajs.react.component.Scala.Component
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.{CtorType, ScalaComponent}
@@ -16,52 +16,25 @@ object DropItemComponent {
       .render_P(render)
       .build
 
-  private val countInput = NumberInputComponent.build(1)
-  private val sourceInput = SelectComponent.build(Depository.inventory.id)
-  private val itemInput = SelectComponent.build[Option[Item.ID]](None)
-
-  private def withSource(player: Player): With[Depository.ID] =
-    render => sourceInput(SelectComponent.Props(
-      id = "target-depository",
-      options = player.depositories.values.map(d => d.id.raw -> d.id).toList,
-      render
-    ))
-
-  private def withItemInput(
-    source: Depository.ID,
-    player: Player,
-    itemCache: ItemCache
-  ): With[Option[Item.ID]] = {
-    val depository = player.depositories(source)
-    val items = itemCache.itemise(depository).map { case (item, _) => item.name -> Some(item.id) }
-
-    render => itemInput(SelectComponent.Props(
-      id = "dropped-item",
-      options = items :+ ("" -> None),
-      render
-    ))
-  }
-
-  private val withCountInput: With[Int] =
-    render => countInput(NumberInputComponent.Props(
-      id = "exp-entry",
-      min = 0,
-      max = Int.MaxValue,
-      step = 1,
-      render
-    ))
-
   private def render(props: AddEffectComponent.Props): VdomNode =
-    withSource(props.player)((source, sourceSelect) =>
-      withItemInput(source, props.player, props.itemCache)((maybeItem, itemInput) =>
-        withCountInput { (count, countInput) =>
-          val maybeEffect = maybeItem.map(item => DropItem(item, count, source))
+    withItemSelection(props.player, props.itemCache) { (maybeItem, itemSelection) =>
+      val maybeEffect =
+        maybeItem
+          .map(selected => DropItem(selected.item, selected.count, selected.source))
+          .filter(_.count > 0)
 
-          FormComponent.build(FormComponent.Props(
-            maybeEffect.map(props.onSubmit).getOrEmpty.when(count > 0).void,
-            formContents = TagMod(sourceSelect, itemInput, countInput)
-          ))
-        }
-      )
-    )
+      FormComponent.build(FormComponent.Props(
+        maybeEffect.map(props.onSubmit).getOrEmpty.void,
+        formContents = itemSelection
+      ))
+    }
+
+  private def withItemSelection(
+    player: Player, itemCache: ItemCache
+  ): ((Option[SelectedItem], TagMod) => VdomNode) => VdomNode =
+    render => SelectItemFromDepositoryComponent.build(SelectItemFromDepositoryComponent.Props(
+      player,
+      itemCache,
+      render
+    ))
 }
