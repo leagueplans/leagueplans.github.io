6e92d34eebc860c72b9395a6e994f76131d46ede
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-05-04 06:02:43 +0100

    #99 [WIP]: Attempt to fix the actions

--- .github/scripts/scrape_wiki.sh
+++ .github/scripts/scrape_wiki.sh
@@ -14,35 +14,26 @@ if [[ ! -d "${RESOURCES}" ]]; then
 fi
 
 run () {
-  local EXTRA_ARGS=$1
-  sbt "wikiScraper/run \"scraper=${SCRAPER}\" \"user-agent=${USER_AGENT}\" \"target-directory=${TMP}\" ${EXTRA_ARGS}"
+  sbt "wikiScraper/run \"scraper=${SCRAPER}\" \"user-agent=${USER_AGENT}\" \"target-directory=${TMP}\" $*"
 }
 
 sync () {
-  local INCLUDES=$1
-
-  echo -vv \
-       --archive \
-       "${INCLUDES}" '--exclude="*"' \
-       --delete \
-       "${TMP}/dump/" "${RESOURCES}/"
-
-
-  rsync -vv \
+  rsync --verbose \
         --archive \
-        "${INCLUDES}" '--exclude="*"' \
         --delete \
+        "$@" \
+        --exclude="*" \
         "${TMP}/dump/" "${RESOURCES}/"
 }
 
 case "${SCRAPER}" in
   "items")
-    run "\"id-map=scraper/src/main/resources/id-map.json\""
-    sync '--include="data/items.json" --include="images/items"'
+    run "id-map=scraper/src/main/resources/id-map.json"
+    sync --include="/data/" --include="/data/items.json" --include="/images/" --include="/images/items/***"
     ;;
 
   "skill-icons")
     run ""
-    sync '--include="/images/" --include="/images/skill-icons/***"'
+    sync --include="/images/" --include="/images/skill-icons/***"
     ;;
 esac
