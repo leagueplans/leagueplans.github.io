cae27d9ba9e604bd024e8be6a722d424189f9224
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-11-24 23:41:57 +0000

    #126: Fix CI

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -9,9 +9,14 @@ function printSbtTask(task) {
       "pipe", // StdIn
       "pipe", // StdOut
       "inherit" // StdErr
-    ]
+    ],
+    encoding: "utf-8"
   };
 
+  // Apparently sbt can output ANSI escape codes. I've only seen one so far,
+  // and it has only ever been printed when running sbt from this bit of Javascript
+  // when running on any GitHub runner. I spent seven hours trying to debug this.
+  // I decided to just look for the problematic character and filter it out.
   const result =
     process.platform === 'win32' ?
       spawnSync("sbt.bat", args.map(x => `"${x}"`), { shell: true, ...options }) :
@@ -22,7 +27,7 @@ function printSbtTask(task) {
   else if (result.status !== 0)
     throw new Error(`sbt process failed with exit code ${result.status}`);
   else
-    return result.stdout.toString('utf8').trim();
+    return result.stdout.toString().replace('[0J', '').trim();
 }
 
 export default defineConfig(({ command, mode }) => {
