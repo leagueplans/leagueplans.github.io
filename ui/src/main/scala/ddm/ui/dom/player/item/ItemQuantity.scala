0d3d1cd0aca679237252a1c927bcb78e2c1ab8e4
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-11-30 23:25:23 +0000

    #131: Laminar ItemQuantity

--- ui/src/main/scala/ddm/ui/dom/player/item/ItemQuantity.scala
+++ ui/src/main/scala/ddm/ui/dom/player/item/ItemQuantity.scala
@@ -1,21 +1,21 @@
 package ddm.ui.dom.player.item
 
-import com.raquo.laminar.api.{L, textToNode}
+import com.raquo.airstream.core.Signal
+import com.raquo.laminar.api.{L, StringValueMapper, textToNode}
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 object ItemQuantity {
-  def apply(quantity: Int): L.Node =
-    quantity match {
-      case q if q <= 1 =>
-        L.emptyNode
-      case q if q < 100000 =>
-        L.span(L.cls(Styles.under100K), q.toString)
-      case q if q < 10000000 =>
-        L.span(L.cls(Styles.under10M), s"${q / 1000}K")
-      case q =>
-        L.span(L.cls(Styles.over10M), s"${q / 1000000}M")
+  def apply(quantity: Signal[Int]): Signal[L.Node] =
+    quantity.splitOne(_ <= 1) {
+      case (true, _, _) => L.emptyNode
+      case (false, _, signal) =>
+        val clsAndRepr = signal.map(classAndRepr)
+        L.span(
+          L.cls <-- clsAndRepr.map(_._1),
+          L.child <-- clsAndRepr.map(_._2)
+        )
     }
 
   @js.native @JSImport("/styles/player/item/itemQuantity.module.css", JSImport.Default)
@@ -24,4 +24,14 @@ object ItemQuantity {
     val under10M: String = js.native
     val over10M: String = js.native
   }
+
+  private def classAndRepr(quantity: Int): (String, String) =
+    quantity match {
+      case q if q < 100000 =>
+        (Styles.under100K, q.toString)
+      case q if q < 10000000 =>
+        (Styles.under10M, s"${q / 1000}K")
+      case q =>
+        (Styles.over10M, s"${q / 1000000}M")
+    }
 }
