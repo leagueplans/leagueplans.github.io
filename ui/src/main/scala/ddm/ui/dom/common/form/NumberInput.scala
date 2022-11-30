570e26469f704f23b9ee078712ba07c62fa5fb32
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-11-30 06:30:45 +0000

    #131: Laminar NumberInput

---
+++ ui/src/main/scala/ddm/ui/dom/common/form/NumberInput.scala
@@ -0,0 +1,33 @@
+package ddm.ui.dom.common.form
+
+import com.raquo.airstream.core.Signal
+import com.raquo.airstream.state.Var
+import com.raquo.laminar.api.{L, eventPropToProcessor}
+
+object NumberInput {
+  def apply[N : Numeric](id: String, initial: N): (L.Input, L.Label, Signal[N]) = {
+    val state = Var(initial)
+    (input(id, initial.toString, state), label(id), state.signal)
+  }
+
+  private def input[N : Numeric](
+    id: String,
+    initial: String,
+    content: Var[N]
+  ): L.Input =
+    L.input(
+      L.`type`("number"),
+      L.idAttr(id),
+      L.name(id),
+      L.defaultValue(initial),
+      L.controlled(
+        L.value <-- content.signal.map(_.toString),
+        L.onInput.mapToValue.map(
+          Numeric[N].parseString(_).getOrElse(Numeric[N].zero)
+        ) --> content
+      )
+    )
+
+  private def label(id: String): L.Label =
+    L.label(L.forId(id))
+}
