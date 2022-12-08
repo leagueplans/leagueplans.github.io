ab6d4dc729b2776be007a07f66d3e5f1348d5d5f
Daniel Moss <daniel.david.moss+git@gmail.com>
2022-12-08 21:36:56 +0000

    #131: Laminar DragSortableList

--- ui/src/main/scala/ddm/ui/facades/fontawesome/freesolid/FreeSolid.scala
+++ ui/src/main/scala/ddm/ui/facades/fontawesome/freesolid/FreeSolid.scala
@@ -6,9 +6,15 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 object FreeSolid {
+  @js.native @JSImport("@fortawesome/free-solid-svg-icons/faEye")
+  object faEye extends IconDefinition
+
   @js.native @JSImport("@fortawesome/free-solid-svg-icons/faEyeSlash")
   object faEyeSlash extends IconDefinition
 
-  @js.native @JSImport("@fortawesome/free-solid-svg-icons/faEye")
-  object faEye extends IconDefinition
+  @js.native @JSImport("@fortawesome/free-solid-svg-icons/faGripVertical")
+  object faGripVertical extends IconDefinition
+
+  @js.native @JSImport("@fortawesome/free-solid-svg-icons/faPlus")
+  object faPlus extends IconDefinition
 }
