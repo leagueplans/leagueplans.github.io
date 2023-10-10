1e2451bfd7ff9f54692daa71471cd3f2368b2a74
Daniel Moss <daniel.david.moss+git@gmail.com>
2023-10-10 22:27:27 +0100

    #146: Close modal on clicking outside the modal

--- ui/src/main/scala/ddm/ui/dom/common/Modal.scala
+++ ui/src/main/scala/ddm/ui/dom/common/Modal.scala
@@ -2,10 +2,14 @@ package ddm.ui.dom.common
 
 import com.raquo.airstream.core.Observer
 import com.raquo.airstream.eventbus.{EventBus, WriteBus}
-import com.raquo.laminar.api.{L, enrichSource, eventPropToProcessor}
+import com.raquo.laminar.api.{L, enrichSource, eventPropToProcessor, seqToModifier}
 import com.raquo.laminar.builders.HtmlTag
 import com.raquo.laminar.nodes.ReactiveHtmlElement
-import org.scalajs.dom.{Event, HTMLDialogElement}
+import org.scalajs.dom.{Event, HTMLDialogElement, MouseEvent}
+import ddm.ui.utils.laminar.LaminarOps.RichL
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
 
 object Modal {
   // Currently does not exist in Laminar
@@ -17,16 +21,29 @@ object Modal {
 
     val node =
       dialog(
-        L.child <-- content.events.map(_.getOrElse(L.emptyNode)),
+        L.cls(Styles.dialog),
+        L.div(
+          L.cls(Styles.container),
+          L.child <-- content.events.map(_.getOrElse(L.emptyNode)),
+        ),
         L.inContext(node =>
-          content.events.map(_.nonEmpty) --> Observer[Boolean] {
-            case true => if (!node.ref.open) node.ref.showModal()
-            case false => if (node.ref.open) node.ref.close()
-          }
+          List(
+            content.events.map(_.nonEmpty) --> Observer[Boolean] {
+              case true => if (!node.ref.open) node.ref.showModal()
+              case false => if (node.ref.open) node.ref.close()
+            },
+            L.ifUnhandledF(L.onClick)(_.filter(_.target == node.ref)) --> content.writer.contramap[MouseEvent](_ => None)
+          )
         ),
         L.customEventProp("close") --> content.writer.contramap[Event](_ => None)
       )
 
     (node, content.writer)
+  }
+
+  @js.native @JSImport("/styles/common/modal.module.css", JSImport.Default)
+  private object Styles extends js.Object {
+    val dialog: String = js.native
+    val container: String = js.native
   }
 }
