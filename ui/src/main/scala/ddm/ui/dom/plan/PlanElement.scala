ae764f7e17e88dbcda7d2f6fdd12697ca9a9e1c6
Daniel Moss <daniel.david.moss+git@gmail.com>
2023-10-24 00:44:29 +0100

    #184: Allow for marking steps as complete

--- ui/src/main/scala/ddm/ui/dom/plan/PlanElement.scala
+++ ui/src/main/scala/ddm/ui/dom/plan/PlanElement.scala
@@ -2,6 +2,7 @@ package ddm.ui.dom.plan
 
 import com.raquo.airstream.core.{Observer, Signal}
 import com.raquo.airstream.eventbus.EventBus
+import com.raquo.airstream.state.{StrictSignal, Var}
 import com.raquo.laminar.api.{L, enrichSource}
 import ddm.ui.dom.common.{ContextMenu, Forester}
 import ddm.ui.model.common.forest.Forest
@@ -18,16 +19,20 @@ object PlanElement {
     stepUpdates: EventBus[Forester[UUID, Step] => Unit],
     focusObserver: Observer[UUID]
   ): (L.Div, Forester[UUID, Step]) = {
+    val allStepsVar = Var(List.empty[UUID])
+    val completionManager = new CompletionManager(allStepsVar.signal)
+
     val forester = Forester[UUID, Step](
       initialPlan,
       _.id,
-      toElement(_, _, _, focusedStep, editingEnabled, contextMenuController, stepUpdates.writer, focusObserver)
+      toElement(_, _, _, focusedStep, completionManager, editingEnabled, contextMenuController, stepUpdates.writer, focusObserver)
     )
 
     val dom =
       L.div(
         L.children <-- forester.domSignal,
-        stepUpdates.events --> Observer[Forester[UUID, Step] => Unit](_.apply(forester))
+        stepUpdates.events --> Observer[Forester[UUID, Step] => Unit](_.apply(forester)),
+        forester.forestSignal.map(_.toList.map(_.id)) --> allStepsVar
       )
 
     (dom, forester)
@@ -38,6 +43,7 @@ object PlanElement {
     step: Signal[Step],
     subSteps: Signal[List[L.Node]],
     focusedStep: Signal[Option[UUID]],
+    completionManager: CompletionManager,
     editingEnabled: Signal[Boolean],
     contextMenuController: ContextMenu.Controller,
     stepUpdater: Observer[Forester[UUID, Step] => Unit],
@@ -47,13 +53,31 @@ object PlanElement {
       stepID,
       step,
       subSteps,
-      Signal.combine(step, focusedStep).map {
-        case (s, Some(focus)) if s.id == focus => StepElement.Theme.Focused
-        case _ => StepElement.Theme.NotFocused
+      isFocused = Signal.combine(step, focusedStep).map {
+        case (s, Some(focus)) => s.id == focus
+        case _ => false
       },
+      isCompleteSignal = completionManager.isCompleteSignal(stepID),
       editingEnabled,
       contextMenuController,
       stepUpdater,
+      completionStatusObserver = Observer[Boolean](completionManager.updateStatus(stepID, _)),
       focusObserver
     )
+
+  private class CompletionManager(allStepsSignal: StrictSignal[List[UUID]]) {
+    private val completedSteps: Var[List[UUID]] = Var(List.empty)
+
+    private val completedStepsSignal: Signal[Set[UUID]] =
+      completedSteps.signal.map(_.toSet)
+
+    def updateStatus(stepID: UUID, isComplete: Boolean): Unit =
+      if (isComplete)
+        completedSteps.set(allStepsSignal.now().takeWhile(_ != stepID) :+ stepID)
+      else
+        completedSteps.update(_.takeWhile(_ != stepID))
+
+    def isCompleteSignal(stepID: UUID): Signal[Boolean] =
+      completedStepsSignal.map(_.contains(stepID))
+  }
 }
