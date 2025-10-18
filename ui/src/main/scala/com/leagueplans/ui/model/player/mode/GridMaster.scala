1aee778b9c6c75b3c48b8ad0b237130f523464aa
Daniel Moss <daniel.david.moss+git@gmail.com>
2025-10-18 21:20:41 +0100

    Update starting stats for Grid Master

--- ui/src/main/scala/com/leagueplans/ui/model/player/mode/GridMaster.scala
+++ ui/src/main/scala/com/leagueplans/ui/model/player/mode/GridMaster.scala
@@ -1,6 +1,6 @@
 package com.leagueplans.ui.model.player.mode
 
-import com.leagueplans.common.model.Skill.Herblore
+import com.leagueplans.common.model.Skill.{Herblore, Hitpoints}
 import com.leagueplans.common.model.{Item, Skill}
 import com.leagueplans.ui.model.plan.{ExpMultiplier, Plan}
 import com.leagueplans.ui.model.player.item.Depository
@@ -12,7 +12,10 @@ object GridMaster extends Mode {
   val settings: Plan.Settings.Explicit =
     Plan.Settings.Explicit(
       initialPlayer = MainGame.initialPlayer.copy(
-        stats = Stats(Herblore -> Level(3).bound),
+        stats = Stats(
+          Herblore -> Level(3).bound,
+          Hitpoints -> Level(10).bound
+        ),
         depositories = MainGame.initialPlayer.depositories + (
           Depository.Kind.Inventory -> Depository(
             MainGame.initialInventory + ((Item.ID(3737), false) -> 1), // Dramen staff
