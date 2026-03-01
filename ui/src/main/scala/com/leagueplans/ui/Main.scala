837402e67dd99edd01a964a6db4f6835c0272c2f
Daniel Moss <daniel.david.moss+git@gmail.com>
2026-03-01 23:10:49 +0000

    Simplify bootstrapping

--- ui/src/main/scala/com/leagueplans/ui/Main.scala
+++ ui/src/main/scala/com/leagueplans/ui/Main.scala
@@ -1,16 +1,9 @@
 package com.leagueplans.ui
 
 import com.leagueplans.ui.dom.Bootstrap
-import com.raquo.laminar.api.{L, eventPropToProcessor}
+import com.raquo.laminar.api.L
 import org.scalajs.dom.document
 
-import scala.annotation.nowarn
-
 @main
-def main(): Unit = {
-  L.documentEvents(_.onDomContentLoaded).foreach { _ =>
-    val container = document.createElement("div")
-    document.body.appendChild(container)
-    L.render(container, Bootstrap())
-  }(using L.unsafeWindowOwner): @nowarn("msg=discarded non-Unit value")
-}
+def main(): Unit =
+  L.renderOnDomContentLoaded(document.body, Bootstrap())
