8b912aacb76db5a87d236f8dcd748cc4967ec434
Daniel Moss <daniel.david.moss+git@gmail.com>
2026-03-02 01:47:17 +0000

    Changelog update

--- ui/src/main/scala/com/leagueplans/ui/dom/landing/changelog/Changelog.scala
+++ ui/src/main/scala/com/leagueplans/ui/dom/landing/changelog/Changelog.scala
@@ -13,41 +13,17 @@ object Changelog {
     L.ol(
       L.cls(Styles.changelog),
       item(
+        new Date(2026, 2, 1),
+        "Preparing for Leagues VI",
+        List(
+          "I'm actively working on UX improvements and new features ahead of the upcoming league. If you'd like to help" +
+            " out with importing quest data, do reach out!"
+        )
+      ),
+      item(
         new Date(2026, 0, 25),
         "Deadman: Annihilation support",
-        List(
-          "Annihilation is now available as a game mode option."
-        )
-      ),
-      item(
-        new Date(2025, 9, 11),
-        "Stat pane rework",
-        List(
-          "The gain-xp modal has been significantly improved. You'll now see a projection of the level you'll end up at," +
-            " and it should be less tedious to add XP to multiple skills at once",
-          "The right-click menus on skill tabs have been removed. You can now just click the skills instead",
-          "XP multipliers can now affect a subset of skills, and be based on combat levels",
-          "The Deadman: Armageddon XP multiplier now reflects the 15x multiplier once the player hits 71 combat",
-          "Fixed an issue where plans would need to be reimported to pick up config changes for game modes"
-        )
-      ),
-      item(
-        new Date(2025, 8, 7),
-        "UX improvements to the inventory",
-        List(
-          "New buttons for adding items to the inventory, banking all items in the inventory, and exporting bank tags",
-          "Spruced up the modals for adding items to the inventory and exporting bank tags",
-          "Part of ongoing work to move away from hidden right-click menus and improve UX"
-        )
-      ),
-      item(
-        new Date(2025, 2, 8),
-        "Support for keyboard shortcuts",
-        List(
-          "This updates adds a few initial keyboard shortcuts to make the process of creating a plan smoother. Give me" +
-            " a ping if you'd like to suggest additional shortcuts.",
-          "Spruced up the modals for adding and deleting steps"
-        )
+        List.empty
       )
     )
 
